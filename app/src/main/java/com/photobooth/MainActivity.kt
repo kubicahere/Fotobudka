1f40bf4c41bce667cf62ecb96f14f4adc8fb4b49
Venrofi <radoslaw.terelak@gmail.com>
2023-02-07 19:52:53 +0100

    Styling updates

--- app/src/main/java/com/photobooth/MainActivity.kt
+++ app/src/main/java/com/photobooth/MainActivity.kt
@@ -51,6 +51,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportActionBar?.hide();
 
         cameraProviderFuture = ProcessCameraProvider.getInstance(this)
         cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
